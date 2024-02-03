add9ef82d1acd61f521527eeb2d8e3c5d2326ba4
Naix <91845220+NaixGames@users.noreply.github.com>
2024-02-03 16:05:43 +0000

    Fixing keepsake item not giving corresponding keepsake access

---
+++ Polycosmos/PolycosmosKeepsakeData.lua
@@ -0,0 +1,76 @@
+ModUtil.Mod.Register( "PolycosmosKeepsakeData" )
+
+
+--------------------------------------
+
+ModUtil.Table.NilMerge( GiftData, {
+		NPC_Achilles_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Orpheus_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Eurydice_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Patroclus_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Nyx_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Hypnos_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_FurySister_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Thanatos_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Dusa_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Skelly_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Sisyphus_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Charon_01 = {
+			UnlockGameStateRequirements = true
+		},
+		NPC_Cerberus_01 = {
+			UnlockGameStateRequirements = true
+		},
+		ZeusUpgrade = {
+			UnlockGameStateRequirements = true
+		},
+		PoseidonUpgrade = {
+			UnlockGameStateRequirements = true
+		},
+		AthenaUpgrade = {
+			UnlockGameStateRequirements = true
+		},
+		AresUpgrade = {
+			UnlockGameStateRequirements = true
+		},
+		DionysusUpgrade = {
+			UnlockGameStateRequirements = true
+		},
+		AphroditeUpgrade = {
+			UnlockGameStateRequirements = true
+		},
+		ArtemisUpgrade = {
+			UnlockGameStateRequirements = true
+		},
+		DemeterUpgrade = {
+			UnlockGameStateRequirements = true
+		},
+		HermesUpgrade = {
+			UnlockGameStateRequirements = true
+		},
+		TrialUpgrade = {
+			UnlockGameStateRequirements = true
+		},
+	})
