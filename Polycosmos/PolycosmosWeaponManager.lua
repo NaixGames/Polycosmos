9fffffb288e65438106dd03c3ce79697cfed2bfb
Naix <91845220+NaixGames@users.noreply.github.com>
2024-02-19 19:01:37 +0000

    Fixing small bugs. Cleaning some extra files that are not needed.

--- Polycosmos/PolycosmosWeaponManager.lua
+++ Polycosmos/PolycosmosWeaponManager.lua
@@ -1,6 +1,33 @@
 ModUtil.Mod.Register( "PolycosmosWeaponManager" )
 
------------- 
+------------ Setup of high level weapon system
+
+-- Checks the weapon being unlocked only if the cosmetics are available.
+ModUtil.Path.Wrap("IsWeaponUnlocked", function(baseFunc, weaponName)
+    if weaponName == "SwordWeapon" and GameState.Cosmetics.SwordWeaponUnlockItem then
+        return true
+    elseif weaponName == "SpearWeapon" and GameState.Cosmetics.SpearWeaponUnlockItem then
+        return true
+    elseif weaponName == "ShieldWeapon" and GameState.Cosmetics.ShieldWeaponUnlockItem then
+        return true
+    elseif weaponName == "BowWeapon" and GameState.Cosmetics.BowWeaponUnlockItem then
+        return true
+    elseif weaponName == "FistWeapon" and GameState.Cosmetics.FistWeaponUnlockItem then
+        return true
+    elseif weaponName == "GunWeapon" and GameState.Cosmetics.GunWeaponUnlockItem then
+        return true    
+    end
+    return false
+end, PolycosmosWeaponManager)
+
+-- Makes it impossible to buy from the Arsenal Room (required since only the Cosmetics unlock the weapons now, so buying them just wastes keys)
+ModUtil.Path.Context.Wrap("ModUtil.Hades.Triggers.OnUsed.Interactables.1.Call", function()
+    ModUtil.Path.Wrap("HasResource", function(baseFunc, name, amount)
+        return false
+    end, PolycosmosWeaponManager)
+end, PolycosmosWeaponManager)
+
+------------
 
 local InitialWeaponRequested = false
 
@@ -85,6 +112,7 @@ ModUtil.Path.Wrap("AddCosmetic", function (baseFunc, name, status)
     end
 
     StyxScribe.Send(styx_scribe_send_prefix.."Locations updated:"..name.."Location")
+    return baseFunc(name, status)
 end)
 
 ------------ 
