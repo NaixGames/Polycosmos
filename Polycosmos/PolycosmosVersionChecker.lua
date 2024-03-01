316903cd587b256925c7f10bd035e3dfdf6207bc
Naix <91845220+NaixGames@users.noreply.github.com>
2024-03-01 19:39:21 +0000

    uploading head revision. Hopefully no one download it on that second.

--- Polycosmos/PolycosmosVersionChecker.lua
+++ Polycosmos/PolycosmosVersionChecker.lua
@@ -1,6 +1,6 @@
 ModUtil.Mod.Register( "PolycosmosVersionChecker" )
 
-polycosmos_version = "0.7.0"
+polycosmos_version = "0.8.0"
 
 -- Auxiliary function to check if an array has a value
 function PolycosmosVersionChecker.CheckVersion(message)
