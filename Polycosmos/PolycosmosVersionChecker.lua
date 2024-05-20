5a0c8043aea199788e192c2e3cf4c441b957991a
Naix <91845220+NaixGames@users.noreply.github.com>
2024-05-20 20:14:18 +0100

    Updating head revision for new enumeration system

--- Polycosmos/PolycosmosVersionChecker.lua
+++ Polycosmos/PolycosmosVersionChecker.lua
@@ -1,6 +1,6 @@
 ModUtil.Mod.Register( "PolycosmosVersionChecker" )
 
-polycosmos_version = "0.10.2"
+polycosmos_version = "0.11"
 
 -- Auxiliary function to check if an array has a value
 function PolycosmosVersionChecker.CheckVersion(message)
