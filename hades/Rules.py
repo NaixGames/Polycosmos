1216afc9ec010f002e01b6ba3231ac79c687c283
Naix <91845220+NaixGames@users.noreply.github.com>
2024-11-22 20:40:22 +0000

    Fiixing some more logic errors. Reminder; in the randomizer progressive items might not be obtain on a progressive way lol

--- hades/Rules.py
+++ hades/Rules.py
@@ -225,10 +225,13 @@ def set_store_rules(world: "HadesWorld", player: int, location_table, options):
     
     #Infernal Trove
     set_rule(world.get_location("Infernal Trove2 Location", player), lambda state: state._has_enough_throve(player,1) \
+        and state.has("Fountain Tartarus Item",player) and state.has("Fountain Asphodel Item",player) \
         and state.has("Fountain Elysium Item",player) and state.has("Keepsake Collection Item", player))
+
     set_rule(world.get_location("Infernal Trove3 Location", player), lambda state: state._has_enough_throve(player,2) \
-        and state.has("Fountain Elysium Item",player) and state.has("Keepsake Collection Item", player) and  \
-            state.has("Deluxe Contractor Desk Item", player))
+        and state.has("Fountain Tartarus Item",player) and state.has("Fountain Asphodel Item",player) \
+        and state.has("Fountain Elysium Item",player) and state.has("Keepsake Collection Item", player) \
+        and state.has("Deluxe Contractor Desk Item", player))
     
     #Keepsake storage
     set_rule(world.get_location("Keepsake Collection Location", player), lambda state:  \
